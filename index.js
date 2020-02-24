21864dd17c782395ea28898c70433e023d31339b
Vernon van der Merwe <Vernon.vd.m14@gmail.com>
2020-02-24 14:56:18 +0200

    Updated the besite, not functional

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.get('/', (req,res)=> {
     res.render('home');
